805db2b1e786920ce21a8e95a026a582b7d1bcf2
colinmccann <colindmccann@gmail.com>
2015-03-26 16:14:18 -0400

    minor css change

--- smartboard/js/smartboard.view.wall.js
+++ smartboard/js/smartboard.view.wall.js
@@ -227,7 +227,7 @@
         // patch was flipping published to false, so we had to remove it. No idea why. Probably something in the faye library?
         // follow the .patch() below to faye-browser.js: this._socket.onmessage (~line 1827) to see where it happens
         // return doc.save(null, { patch: true })
-        return doc.save()
+        return doc.save();
       });
       view.$el.on('drag', function(ev, ui) {
         if (view.renderConnectors !== null) {
